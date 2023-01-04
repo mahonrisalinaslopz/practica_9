21299cd8cb0a1edee9470249bd2bdfd2f919577a
programmertocode <children_park@hotmail.com>
2023-01-04 09:23:26 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useState } from "react";
+import { render } from "react-dom";
+import "./App.css";
+const THEMES = {
+  dark: {
+    background: "#000",
+    color: "#fff"
+  },
+  light: {
+    background: "#fff",
+    color: "#000"
+  }
+};
+const ThemeContext = React.createContext(THEMES.dark);
 
-function App() {
+export default function App() {
+  const [theme, setTheme] = useState("light");
+  const toggleTheme = useCallback(function () {
+    setTheme((t) => (t === "light" ? "dark" : "light"));
+  }, []);
+  const currentTheme = theme === "light" ? THEMES.light : THEMES.dark;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ThemeContext.Provider value={currentTheme}>
+        <Toolbar  />
+      </ThemeContext.Provider>
+      <button onClick={toggleTheme} >Change Theme</button>
     </div>
   );
 }
 
-export default App;
+function ThemeButton({ children }) {
+  const value = React.useContext(ThemeContext);
+  return <button style={value}>{children} </button>;
+}
+
+// The class way :
+class ThemedButtonClass extends React.Component {
+  render() {
+    return (
+      <ThemeContext.Consumer>
+        {(value) => {
+          return <button style={value}>{this.props.children} </button>;
+        }}
+      </ThemeContext.Consumer>
+    );
+  }
+}
+
+function Toolbar({ theme }) {
+  return (
+    <div>
+      <SearchForm />
+      <ThemeButton style={theme}>Subscribe</ThemeButton>
+    </div>
+  );
+}
+
+function SearchForm() {
+  return (
+    <div>
+      <input type="text" />
+      <ThemedButtonClass>Search</ThemedButtonClass>
+    </div>
+  );
+}
